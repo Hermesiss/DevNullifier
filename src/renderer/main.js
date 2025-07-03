ff10fc0e5ca6dc984aef83918c7d186e6e6caf69
Hermesis <hermesiss@gmail.com>
2025-07-03 11:35:10 +0200

    Remove unused VTreeview

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import { VTreeview } from "vuetify/labs/VTreeview";
 import "vuetify/styles";
 import "@mdi/font/css/materialdesignicons.css";
 
@@ -10,8 +9,7 @@ import App from "./App.vue";
 
 const vuetify = createVuetify({
   components: {
-    ...components,
-    VTreeview
+    ...components
   },
   directives,
   theme: {
